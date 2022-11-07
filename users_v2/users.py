d39c3c5186abb0fde9eb83043b5836f4f7d363eb
Tamkin Anwar <tamkinanwar7@gmail.com>
2022-11-07 17:38:03 -0500

    update_2.6

---
+++ users_v2/users.py
@@ -0,0 +1,23 @@
+class User:
+
+    def __init__(self, first_name, last_name, email, age):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0
+
+    def display_info(self):
+        print("==========================")
+        print(f"First name: {self.first_name}")
+        print(f"Last name: {self.last_name}")
+        print(f"Email: {self.email}")
+        print(f"Age: {self.age}")
+        print(f"Member: {self.is_rewards_member}")
+        print(f"Current Points: {self.gold_card_points}")
+        print("==========================")
+
+my_user = User("Tamkin", "Anwar", "tamkinanwar7@gmai.com", 24)
+my_user.display_info()
