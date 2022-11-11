2d6aa5d1e24814cb95f966d56ae111a036654183
Tamkin Anwar <tamkinanwar7@gmail.com>
2022-11-10 21:49:25 -0500

    update_2.8

---
+++ basketball_dic_v2/basketball_dic_v2.py
@@ -0,0 +1,64 @@
+players = [
+    {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+    },
+    {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+    },
+    {
+        "name": "Damian Lillard", 
+        "age":33, "position": "Point Guard", 
+        "team": "Portland Trailblazers"
+    },
+    {
+        "name": "Joel Embiid", 
+        "age":32, "position": "Power Foward", 
+        "team": "Philidelphia 76ers"
+    },
+    {
+        "name": "", 
+        "age":16, 
+        "position": "P", 
+        "team": "en"
+    }
+]
+
+class Player:
+    def __init__(self, name, age, position, team):
+        self.name = name
+        self.age = age
+        self.position = position
+        self.team = team
+
+kevin = {
+    "name": "Kevin Durant", 
+    "age":34, 
+    "position": "small forward", 
+    "team": "Brooklyn Nets"
+}
+jason = {
+    "name": "Jason Tatum", 
+    "age":24, 
+    "position": "small forward", 
+    "team": "Boston Celtics"
+}
+kyrie = {
+    "name": "Kyrie Irving", 
+    "age":32, "position": "Point Guard", 
+    "team": "Brooklyn Nets"
+}
+    
+# Create your Player instances here!
+# player_jason = ???
+
